e4ad46c36a2a0cc2ba6ffbc17d93c6a3a8db89d0
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-29 15:38:31 +0530

    initial commit

--- LearnJs/loop.js
+++ LearnJs/loop.js
@@ -1,7 +1,15 @@
-let sum = 0;
-i = 1;
-while(i <= 1024){
-     sum = sum+i;
-     i++;
+// let sum = 0;
+i = 5;
+// while(i <= 1024){
+//      sum = sum+i;
+//      i++;
+// }
+// console.log(sum);
+
+const countDown = [];
+while(i >= 1){
+     countDown.push(i);
+    i--;
 }
-console.log(sum);
+
+console.log(countDown[2]);
