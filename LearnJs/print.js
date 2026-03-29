f325111151a108ce5513fbe94f09005fbf1c2329
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-29 12:21:55 +0530

    initial commit

--- LearnJs/print.js
+++ LearnJs/print.js
@@ -1,12 +1,32 @@
-console.log("Hello");
-process.stdout.write("Mohit");
-process.stdout.write("Mohit\n");
+// console.log("Hello");
+// process.stdout.write("Mohit");
+// process.stdout.write("Mohit\n");
 
-// console.clear();
+// // console.clear();
 
-console.log("max");
-console.table({city:"Panipat"});
+// console.log("max");
+// console.table({city:"Panipat"});
+// var score = 123;
+// console.log(score);
 
-var score = 123;
- score = 122;
-console.log(score);
+
+// let gameName = "Takken";
+// gameName = "Street Fighter"
+// console.log(gameName);
+
+// const Name = "Mohit";
+// Name = "Max";
+
+// console.log(Name);
+
+
+const number = 56;
+const number1 = 56;
+const newNum = new Number(56);
+
+
+const name = "Mohit";
+const name1 = "Mohit";
+const nmae = new String("Mohi");
+const nme = new String("Mohi");
+console.log(name === name1);
