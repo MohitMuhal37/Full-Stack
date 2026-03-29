5a8ec86e2de4aec71fdcfc8802186cc313b3b4fa
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-29 15:58:03 +0530

    initial commit

--- LearnJs/loop.js
+++ LearnJs/loop.js
@@ -16,9 +16,16 @@ console.log(countDown[2]);
 
 let pmt;
 const tea =[];
-do{
-    pmt = prompt("Enter fav tea = ");
-    if(pmt !== "stop"){
-        tea.push(pmt);
-    }
-}while(pmt !== "stop");
+// do{
+//     pmt = prompt("Enter fav tea = ");
+//     if(pmt !== "stop"){
+//         tea.push(pmt);
+//     }
+// }while(pmt !== "stop");
+
+const arr = [2, 4, 6];
+let multiply = [];
+for(let i = 0; i < arr.length; i++){
+    multiply = arr[i] * 2;
+    console.log(multiply); 
+}
