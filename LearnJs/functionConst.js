8f642de3abcbd870a42c5cffe2d4abde93b8aa23
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-30 19:07:17 +0530

    initial commit

--- LearnJs/functionConst.js
+++ LearnJs/functionConst.js
@@ -10,3 +10,14 @@ function Car(make, model){
 
 let myCar = new Car('Toyota', 'Camry')
 console.log(myCar);
+
+
+function Tea(type){
+    this.type = type;
+    this.description = () => {
+        return `this is a cup of ${this.type}`
+    }
+}
+
+const lemonTea = new Tea("Green");
+console.log(lemonTea.description());
