afdfbd60d4d8e9973f880be328556899220b9644
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-03-30 20:17:23 +0530

    initial commit

---
+++ LearnJs/OopMaster.js
@@ -0,0 +1,20 @@
+class Vehicle {
+    constructor(model, make){
+        this.model = model;
+        this.make = make;
+    }
+
+    start(){
+        return `This ${this.model} can start`
+    }
+}
+
+class Car extends Vehicle{
+    drive(){
+        return `${this.model}, model no of ${this.make} this is Moving`
+    }
+}
+
+const  c1 = new Car("toyota", "2025");
+console.log(c1.start());
+console.log(c1.drive())
--- LearnJs/functionConst.js
+++ LearnJs/functionConst.js
@@ -1,23 +1,39 @@
-function Person(name, age){
-    this.name = name
-    this.age = age
+// function Person(name, age){
+//     this.name = name
+//     this.age = age
+// }
+
+// function Car(make, model){
+//     this .make = make
+//     this.model = model
+// }
+
+// let myCar = new Car('Toyota', 'Camry')
+// console.log(myCar);
+
+
+// function Tea(type){
+//     this.type = type;
+//     this.description = () => {
+//         return `this is a cup of ${this.type} tea`
+//     }
+// }
+
+// const lemonTea = new Tea("Green");
+// console.log(lemonTea.description());
+
+
+function Species(Sname){
+        this.Sname = Sname;
 }
 
-function Car(make, model){
-    this .make = make
-    this.model = model
+Species.prototype.Sound = function(){
+    return `Every ${this.Sname} can make sound`;
 }
 
-let myCar = new Car('Toyota', 'Camry')
-console.log(myCar);
-
-
-function Tea(type){
-    this.type = type;
-    this.description = () => {
-        return `this is a cup of ${this.type}`
-    }
+Species.prototype.Eat = function(){
+    return `Every ${this.Sname} can eat`
 }
 
-const lemonTea = new Tea("Green");
-console.log(lemonTea.description());
+const animal = new Species("Dog");
+console.log(animal.Eat());
