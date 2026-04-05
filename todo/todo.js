01c1b3e13602d6cabf14a25ee79eb19f9704b721
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-05 17:01:09 +0530

    initial commit

--- todo/todo.js
+++ todo/todo.js
@@ -1,11 +1,11 @@
-const inp = document.getElementById("todo-input");
+document.addEventListener('DOMContentLoaded', () => {
+  const inp = document.getElementById("todo-input");
 const inpBtn = document.getElementById("add-task-btn");
 const ulList = document.getElementById("todo-list");
 
 
-let task = [];
-
-function addTask()
+let tasks = JSON.parse(localStorage.getItem("task")) || [];
+const  addTask = () => 
 {
   const taskText = inp.value.trim();
   if(taskText === "")return;
@@ -15,9 +15,27 @@ function addTask()
     task : taskText,
     isCompleted : false
   }
-  task.push(newTask);
+  tasks.push(newTask);
+  saveTask();
   inp.value = "" //clear input
-  console.log(task);
+  console.log(tasks);
   
 }
+
+saveTask = () => {
+    localStorage.setItem("task", JSON.stringify(task));
+}
+
+function rennderTask(task) {
+  const li = document.createElement('li');
+  li.setAttribute("data-id", task.id);
+  if(task.comleted) li.classList.add('completed')
+  li.innerHTML =`<span>
+  ${task.text}
+  </span>
+  `;
+  ulList.appendChild(li);
+}
+tasks.forEach((task) => rennderTask(task))
 inpBtn.addEventListener("click", addTask)
+})
