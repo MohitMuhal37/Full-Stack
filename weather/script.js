04b8865888fef2cf10648c2b3ea216b372c83c2f
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-09 19:02:26 +0530

    initial commit

---
+++ weather/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const cityInput = document.getElementById('city-input');
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const weatherInfo = document.getElementById("weather-info");
+    const cityName = document.getElementById("city-name");
+    const temprature = document.getElementById("temerature");
+    const descriptions = document.getElementById("description");
+    const errorMsg = document.getElementById("error-message");
+});
