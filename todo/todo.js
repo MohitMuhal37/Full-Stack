190ee3c6cef41216d074905ab7e641dc34d93720
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-11 18:18:25 +0530

    initial commit

--- todo/todo.js
+++ todo/todo.js
@@ -1,41 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const inp = document.getElementById("todo-input");
-const inpBtn = document.getElementById("add-task-btn");
-const ulList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let tasks = JSON.parse(localStorage.getItem("task")) || [];
-const  addTask = () => 
-{
-  const taskText = inp.value.trim();
-  if(taskText === "")return;
+  tasks.forEach((task) => renderTask(task));
 
-  const newTask = {
-    id : Date.now(),
-    task : taskText,
-    isCompleted : false
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return;
+
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    renderTask(newTask);
+    todoInput.value = ""; //clear input
+    console.log(tasks);
+  });
+
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>
+    `;
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = !task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation(); //prevent toggle from firing
+      tasks = tasks.filter((t) => t.id === task.id);
+      li.remove();
+      saveTasks();
+    });
+
+    todoList.appendChild(li);
   }
-  tasks.push(newTask);
-  saveTask();
-  inp.value = "" //clear input
-  console.log(tasks);
-  
-}
 
-saveTask = () => {
-    localStorage.setItem("task", JSON.stringify(task));
-}
-
-function rennderTask(task) {
-  const li = document.createElement('li');
-  li.setAttribute("data-id", task.id);
-  if(task.comleted) li.classList.add('completed')
-  li.innerHTML =`<span>
-  ${task.text}
-  </span>
-  `;
-  ulList.appendChild(li);
-}
-tasks.forEach((task) => rennderTask(task))
-inpBtn.addEventListener("click", addTask)
-})
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
--- weather/script.js
+++ weather/script.js
@@ -1,67 +1,58 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    const cityInput = document.getElementById('city-input');
-    const getWeatherBtn = document.getElementById("get-weather-btn");
-    const weatherInfo = document.getElementById("weather-info");
-    const cityName = document.getElementById("city-name");
-    const temprature = document.getElementById("temerature");
-    const descriptions = document.getElementById("description");
-    const errorMsg = document.getElementById("error-message");
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-messag");
 
-    const API_KEY = "8853b2dc70d00dd6133941fe0d1468e7"; 
+  const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"; //env variables
 
+  getWeatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
 
-   async function fetchweatherData(city)
-    {
-        //get the data
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-        const response = await fetch(url);
-        console.log(typeof(response));
-        console.log("Response",response);
-        
-        if(!response.ok){
-            throw new Error("city not found");
-        }
-        const data = await response.json();
-        return data;
+    // it may throw an error
+    // server/database is always in another continent
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
     }
+  });
 
+  async function fetchWeatherData(city) {
+    //gets the data
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
-    function displayWeatherData(data)
-    {
-        console.log(data);
-        const {name, main, weather} = data;
-        cityName.textContent = name;
+    const response = await fetch(url);
+    console.log(typeof response);
+    console.log("RESPONSE", response);
 
-        temprature.textContent = `Temperature : ${main.temp}`
-        descriptions.textContent = `Description : ${weather[0].description}`
-
-         weatherInfo.classList.remove("hidden");
-        errorMsg.classList.add("hidden");
+    if (!response.ok) {
+      throw new Error(" City Not found");
     }
+    const data = await response.json();
+    return data;
+  }
 
-    function showError()
-    {
-        weatherInfo.classList.remove("hidden");
-        errorMsg.classList.add("hidden");
-    }
+  function displayWeatherData(data) {
+    console.log(data);
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
+    descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
 
-    async function getWeather(){
-    const  city = cityInput.value.trim();
-    if(!city)return; 
-     //server may throw an error
-     //server/database is always in another continent
+    //unlock the display
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
 
-     try
-     {
-       const weatherData = await fetchweatherData(city);
-       displayWeatherData(weatherData);
-     }catch(error)
-     {
-
-     }
-
-
-    } 
-
-    getWeatherBtn.addEventListener("click", getWeather);
+  function showError() {
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
 });
