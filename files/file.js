a51b1772855da7f8546663979e43bbf979a18b43
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-19 19:23:23 +0530

    initial commit

--- files/file.js
+++ files/file.js
@@ -1,2 +1,19 @@
+const { log } = require("console");
 const fs = require("fs");
 const filePathe = "./tasks.json";
+
+
+const addTasks
+
+const command = process.argv[2];
+const args = process.argv[3];
+
+if(command === "add"){
+    addTasks(args);
+}else if(command === "list"){
+    listTasks();
+}else if(command === "remove"){
+    removeTasks(parseInt(args));
+}else{
+    console.log("Command not found");
+}
