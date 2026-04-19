7540c080d9c350dfdacd14adf8b2d6d51d0636e4
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-19 19:30:06 +0530

    initial commit

--- files/file.js
+++ files/file.js
@@ -1,9 +1,29 @@
 const { log } = require("console");
 const fs = require("fs");
-const filePathe = "./tasks.json";
+const filePath = "./tasks.json";
 
+const loadTask = () => {
+    try{
+        const dataBuffer = fs.readFileSync(filePath);
+        const jsaonData = dataBuffer.toString();
+        return JSON.parse(jsaonData);
+    }catch(error){
+        return [];
+    }
+}
 
-const addTasks
+const SaveTasks = (task) => {
+    const jsonData = JSON.stringify(task);
+    fs.writeFileSync(filePath, jsonData);
+}
+
+const addTasks = (task) => {
+    const Tasks = loadTask();
+    Tasks.push({task});
+    SaveTasks(Tasks);
+    console.log(`${task}, ${Tasks}`);
+    
+}
 
 const command = process.argv[2];
 const args = process.argv[3];
