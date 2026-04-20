72115da44862b1fbad666d43c52f7b132504821b
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-20 20:06:09 +0530

    initial commit

--- nginxServer/server.js
+++ nginxServer/server.js
@@ -5,17 +5,29 @@ const path = require("path");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, req.url === "/" ? "index.html":"req.url");
+    const filePath = path.join(__dirname, req.url === "/" ? "index.html":req.url);
     const extname = String(path.extname(filePath).toLowerCase());
 
     const mineType = {
         ".html" : "text/html",
         ".css" : "text/css",
         ".js" : "text/javascript",
-        ".png" : "text/png",
+        ".png" : "image/png",
     }
 
     const contentType = mineType[extname] || "application/octet-stream";
+
+    fs.readFile(filePath, (err,context)=>{
+        if(err){
+            if(err.code === "ENOENT"){
+                res.writeHead(404, {"content-type":"text/html"})
+                res.end("404 FILE NOT FIND BRO")
+            }
+        }else{
+            res.writeHead(200,{"content-type" : contentType});
+            res.end(context, "utf-8")
+        }
+    })
 }); 
 
 server.listen(port, () => {
