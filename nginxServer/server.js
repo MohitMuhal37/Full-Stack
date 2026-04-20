5003e866cfd33d1340f9100004ae47a5ce319b2c
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-20 19:47:00 +0530

    initial commit

--- nginxServer/server.js
+++ nginxServer/server.js
@@ -4,7 +4,19 @@ const path = require("path");
 
 const port = 3000;
 
-const server = http.createServer(); 
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, req.url === "/" ? "index.html":"req.url");
+    const extname = String(path.extname(filePath).toLowerCase());
+
+    const mineType = {
+        ".html" : "text/html",
+        ".css" : "text/css",
+        ".js" : "text/javascript",
+        ".png" : "text/png",
+    }
+
+    const contentType = mineType[extname] || "application/octet-stream";
+}); 
 
 server.listen(port, () => {
     console.log(`Server is Listening on port ${3000}`);
