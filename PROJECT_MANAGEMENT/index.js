5c7ff077514d712103fa9c86b059b25e0de39b9e
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-22 19:27:29 +0530

    initial commit

--- PROJECT_MANAGEMENT/index.js
+++ PROJECT_MANAGEMENT/index.js
@@ -4,7 +4,7 @@ dotenv.config({
     path:"./.env"
 });
 
-const my_User = process.env.username
-console.log(my_User);
+let my_User = process.env.database
+console.log(my_User)
 
 console.log("new backend project");
