fbb018c579a9fbf652c76d90776ef8b866d02dc4
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-25 19:06:29 +0530

    initial commit

---
+++ PROJECT_MANAGEMENT/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json({limit : "16kb"}));
+app.use(express.urlencoded({extended : true, limit : "16kb"}));
+app.use(express.static("public"));
+
+// cors configrations
+app.use(cors({
+    origin : process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods: ["GET","PUT", "PATCH", "DELETE","POST", "HEAD" , "OPTIONS"
+    ],
+    allowedHeaders:["Authorization", "Content-Type"],
+}));
+
+app.get("/",(req, res) => {
+    res.send("Hello Backend");
+});
+
+export default app;
