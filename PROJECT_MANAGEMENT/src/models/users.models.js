c901750c3b4e8c293ac0374ae280f95f6cd8227f
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-27 14:41:24 +0530

    initial commit

--- PROJECT_MANAGEMENT/src/models/users.models.js
+++ PROJECT_MANAGEMENT/src/models/users.models.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
     {
         avatar : {
@@ -55,8 +55,14 @@ const userSchema = new Schema(
             type : Date
         }
   },{
-    timeStamps : true,
+    timeStamps : true, 
   }
 );
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
 
 export const User = mongoose.model("User", userSchema);
