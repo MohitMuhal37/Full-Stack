3f6019505c22ba8b966294ae547591f94790870d
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-04-27 15:35:51 +0530

    initial commit

--- PROJECT_MANAGEMENT/src/models/users.models.js
+++ PROJECT_MANAGEMENT/src/models/users.models.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
     {
         avatar : {
@@ -68,5 +69,29 @@ userSchema.pre("save", async function (next){
 userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password)
 } 
+
+userSchema.methods.generateAcessToken = function(){
+    return jwt.sign(
+        {
+        _id : this._id,
+        email : this.email,
+        username : this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+)
+},
+
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+        id : this._id,
+        email : this.email,
+        username : this.username 
+        },
+         process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn : process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
