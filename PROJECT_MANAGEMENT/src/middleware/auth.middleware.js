9d54672a646db85a3b265ec345fceee285df54e1
Mohit Muhal <mohitmuhal15122003@gmail.com>
2026-05-19 17:26:02 +0530

    add a new file

--- PROJECT_MANAGEMENT/src/middleware/auth.middleware.js
+++ PROJECT_MANAGEMENT/src/middleware/auth.middleware.js
@@ -1,7 +1,17 @@
 import {User} from "../models/users.models.js";
-import {ApiErro} from "../utils/api-Errors.js";
+import {ApiError} from "../utils/api-Errors.js";
 import asynHandler from "../utils/asyncHandler.js";
 
 export const verifyJwt = asynHandler(async(req, resizeBy, next) => {
-    req.cookies?.accessToken || req.header("Authorization")?.replace
+    req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "");
+
+    if(!token){
+        throw new ApiError(401, "unauthorized request")
+    }
+
+    try{
+        jwt.verify(token, process.env.Access_Token_Secret);
+    }catch(){
+
+    }
 })
